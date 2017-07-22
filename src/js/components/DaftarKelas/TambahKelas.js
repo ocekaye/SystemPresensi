f036829d26e234b9da2a7727769e4c3297efa70a
Unknown <ariefta.damas@gmail.com>
2017-07-22 17:43:29 +0700

    Edit 1 form

--- src/js/components/DaftarKelas/TambahKelas.js
+++ src/js/components/DaftarKelas/TambahKelas.js
@@ -19,100 +19,38 @@ export default class TambahKelas extends React.Component {
 
                 <form className="form-horizontal" action="#">
                   <fieldset className="content-group">
-                    <legend className="text-bold">Basic inputs</legend>
+                    <legend className="text-bold">Tambah Data Kelas</legend>
 
                     <div className="form-group">
-                      <label className="control-label col-lg-2">Default text input</label>
+                      <label className="control-label col-lg-2">Nama Kelas</label>
                       <div className="col-lg-10">
                         <input type="text" className="form-control"/>
                       </div>
                     </div>
 
                     <div className="form-group">
-                      <label className="control-label col-lg-2">Password</label>
+                      <label className="control-label col-lg-2">Deskripsi</label>
                       <div className="col-lg-10">
-                        <input type="password" className="form-control"/>
+                        <input type="text" className="form-control"/>
                       </div>
                     </div>
 
                     <div className="form-group">
-                      <label className="control-label col-lg-2">Input with placeholder</label>
-                      <div className="col-lg-10">
-                        <input type="text" className="form-control" placeholder="Enter your username..."/>
-                      </div>
-                    </div>
-
-                    <div className="form-group">
-                      <label className="control-label col-lg-2">Read only field</label>
-                      <div className="col-lg-10">
-                        <input type="text" className="form-control" readonly="readonly" value="read only"/>
-                      </div>
-                    </div>
-
-                    <div className="form-group">
-                      <label className="control-label col-lg-2">Disabled field</label>
-                      <div className="col-lg-10">
-                        <input type="text" className="form-control" disabled="disabled" value="disabled"/>
-                      </div>
-                    </div>
-
-                    <div className="form-group">
-                      <label className="control-label col-lg-2">Predefined value</label>
-                      <div className="col-lg-10">
-                        <input type="text" className="form-control" value="http://"/>
-                      </div>
-                    </div>
-
-                    <div className="form-group">
-                      <label className="control-label col-lg-2">Default select</label>
+                      <label className="control-label col-lg-2">Tag Kelas</label>
                       <div className="col-lg-10">
                         <select name="select" className="form-control">
-                          <option value="opt1">Usual select box</option>
-                          <option value="opt2">Option 2</option>
-                          <option value="opt3">Option 3</option>
-                          <option value="opt4">Option 4</option>
-                          <option value="opt5">Option 5</option>
-                          <option value="opt6">Option 6</option>
-                          <option value="opt7">Option 7</option>
-                          <option value="opt8">Option 8</option>
+                          <option value="opt1">Satu A</option>
+                          <option value="opt2">Satu B</option>
+                          <option value="opt3">Satu C</option>
+                          <option value="opt4">Satu D</option>
+                          <option value="opt5">Satu E</option>
+                          <option value="opt6">Satu F</option>
+                          <option value="opt7">Satu G</option>
+                          <option value="opt8">Saty H</option>
                         </select>
                       </div>
                     </div>
-
-                    <div className="form-group">
-                      <label className="control-label col-lg-2">Disabled autocomplete</label>
-                      <div className="col-lg-10">
-                        <input type="text" className="form-control" placeholder="Autocomplete is off" autocomplete="off"/>
-                      </div>
-                    </div>
-
-                    <div className="form-group">
-                      <label className="control-label col-lg-2">Maximum value</label>
-                      <div className="col-lg-10">
-                        <input type="text" className="form-control" maxlength="4" placeholder="Maximum 4 characters"/>
-                      </div>
-                    </div>
-
-                    <div className="form-group">
-                      <label className="control-label col-lg-2 cursor-pointer" for="clickable-label">Focus on label click</label>
-                      <div className="col-lg-10">
-                        <input type="text" className="form-control" id="clickable-label" placeholder="Field focus on label click"/>
-                      </div>
-                    </div>
-
-                    <div className="form-group">
-                      <label className="control-label col-lg-2">Static text</label>
-                      <div className="col-lg-10">
-                        <div className="form-control-static">This is a static text</div>
-                      </div>
-                    </div>
-
-                    <div className="form-group">
-                      <label className="control-label col-lg-2">Textarea</label>
-                      <div className="col-lg-10">
-                        <textarea rows="5" cols="5" className="form-control" placeholder="Default textarea"></textarea>
-                      </div>
-                    </div>
+                    
                   </fieldset>
                   </form>
               </div>
